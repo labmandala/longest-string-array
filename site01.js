1aa3522e304daca4fdeb369aac39cb2ba4d42a5c
sarah <skg@neofilm.us>
2022-09-06 16:33:08 -0400

    Display all the heroes to the page

--- site01.js
+++ site01.js
@@ -25,14 +25,14 @@ function findHero() {
     document.getElementById("results").innerHTML = lword;
 
     //extra credit display all of the heroes to the page
-
+    document.getElementById("namelist").innerHTML = marvelHeroes.join(" | ");
 }
 
 //takes an array of strings and returns the longest one. 
 function findLongestString(namesArry) {
 
     // declare variable
-    let lstring = [];
+    let lstring = "";
 
     for (let index = 0; index < namesArry.length; index++) {
         if (namesArry[index].length > lstring.length) {
